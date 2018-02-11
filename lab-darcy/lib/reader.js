0bcd33ff83a8f1a7350640c8042766a5744a38c2
dknore <darcy.knore@gmail.com>
2018-02-10 16:51:07 -0800

    added test files

--- lab-darcy/lib/reader.js
+++ lab-darcy/lib/reader.js
@@ -2,37 +2,33 @@
 
 const fs = require('fs');
 
-const pygmalion = require('../Pygmalion.txt');
-const mySecretLife = require('../MySecretLife.txt');
-const letter = require('../Letter.txt');
+// const pygmalion = require('../assets/Pygmalion.txt');
+// const mySecretLife = require('../MySecretLife.txt');
+// const letter = require('../Letter.txt');
 
-
-function readerCb(err, results) {
-    console.log("file 1 was:", results[0]);
-    console.log("file 1 was:", results[1]);
-    console.log("file 1 was:", results[2]);
-}
-
-function readAllFiles(["file1", "file2", "file3"], readerCb) {
-
-    fs.readFile('../Pygmalion.txt', (err, data) => {
-        if (err) throw err;
-        let str = data.toString();
-        console.log(str);
-    });
-
-//     fs.readFile('../MySecretLife.txt', (err, data) => {
-//         if (err) throw err;
-//         let str = data.toString();
-//         console.log(str);
-//     });
-
-//     fs.readFile('../Letter.txt', (err, data) => {
-//         if (err) throw err;
-//         let str = data.toString();
-//         console.log(str);
-//     });
+// function readerCb(err, results) {
+//     if(err) throw err;
+//     console.log("file 1 was:", results[0]);
+//     console.log("file 2 was:", results[1]);
+//     console.log("file 3 was:", results[2]);
 // }
 
+// const assets = ["../assets/Pygmalion.txt", "../assets/MySecretLife.txt", "../assets/Letter.txt"];
+const assets = ["../assets/small.txt", "../assets/medium.txt", "../assets/large.txt"];
 
+function readAllFiles(paths, readerCb) {
+    let results = [];
 
+    for(let i = 0; i < paths.length; i++) {
+        fs.readFile(paths[i], (err, data) => {
+            if (err) throw err;
+            let str = data.toString();
+            results.push(str);
+            console.log("file=", str);
+        });
+    }
+    if(paths.length === results.length) {
+        console.log("results= ", results);
+    }
+}
+readAllFiles(assets);
