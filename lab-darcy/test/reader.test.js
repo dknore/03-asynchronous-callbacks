20ad096c721b32dab565109520001e3c271c11f9
dknore <darcy.knore@gmail.com>
2018-02-11 23:49:30 -0800

    started scaffolding for tests but not working

--- lab-darcy/test/reader.test.js
+++ lab-darcy/test/reader.test.js
@@ -4,18 +4,20 @@ const readAllFiles = require('../lib/reader.js');
 
 describe("readAllFiles function", () => {
     it("should ensure the function rejects errors with invalid file paths.", () => {
-        // let expected = ;
-        // let array = "undefined";
-        // let result = forEach(array);
-        // expect(result).toBe(expected);
+        let expected = "error";
+        let path = "./wrongpath.txt";
+        let result = forEach(path);
+        expect(result).toBe(expected);
     })
     it("should ensure the function correctly resolves mapped string data for an array of file paths.", () => {
-        // let array = "undefined";
-        // let result = forEach(array);
-        // expect(result).toBe(expected);
+        let expected = [""];
+        let path = ["../assets/small.txt", "../assets/medium.txt", "../assets/large.txt"];
+        let result = forEach(path);
+        expect(result).toBe(expected);
     })
     it("should make sure the order of the results matches the order of the files as they were passed in.", () => {
-        // let array = "undefined";
+        // let exptected = 
+        // let array = 
         // let result = forEach(array);
         // expect(result).toBe(expected);
     })
